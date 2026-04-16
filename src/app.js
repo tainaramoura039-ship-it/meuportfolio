1c40f6083ebd2e9006a9bf4a8611bc4ff957cb89
tainaramoura039-ship-it <tainaramoura039@gmail.com>
2026-04-16 20:21:22 -0300

    app.js

---
+++ src/app.js
@@ -0,0 +1,12 @@
+import "./styles.css";
+import Header from "./Header";
+import Projetos from "./Projeto";
+
+export default function App() {
+  return (
+    <div>
+      <Header />
+      <Projetos />
+    </div>
+  );
+}
